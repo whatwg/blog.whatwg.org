19ebdd4e4b5c990d5895ed3a00b637cbf59dc476
Anne van Kesteren <annevk@annevk.nl>
2019-11-05 17:35:06 +0100

    Remove Google Analytics
    And more HTTPS.

--- org.whatwg.awesome/footer.php
+++ org.whatwg.awesome/footer.php
@@ -1,17 +1,11 @@
 <footer>
 	<p>
 		<?php bloginfo('name'); ?> is proudly powered by
-		<a href="http://wordpress.org/">WordPress</a>
+		<a href="https://wordpress.org/">WordPress</a>
 		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
 		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
 		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
 	</p>
 </footer>
-<script src=//www.google-analytics.com/ga.js></script>
-<script>
-try {
-var pageTracker = _gat._getTracker("UA-584304-2");
-pageTracker._trackPageview();
-} catch(err) {}</script> 
 </body>
 </html>
