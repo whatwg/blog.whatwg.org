cf6cf4c017ed49486e7eedcad43442c65d91fb9b
Domenic Denicola <d@domenic.me>
2021-05-17 14:10:54 -0400

    Improve sidebar links and general metadata

--- org.whatwg.awesome/sidebar.php
+++ org.whatwg.awesome/sidebar.php
@@ -2,12 +2,12 @@
  <h2>Pages</h2>
  <ul>
   <li><a href="https://whatwg.org/">Home</a></li>
-  <li><a href="https://wiki.whatwg.org/wiki/FAQ">FAQ</a></li>
-  <li><a href="https://wiki.whatwg.org/wiki/IRC">IRC</a></li>
-  <li><a href="https://wiki.whatwg.org/wiki/Code_of_Conduct">Code of Conduct</a></li>
+  <li><a href="https://whatwg.org/faq">FAQ</a></li>
+  <li><a href="https://whatwg.org/irc">IRC</a></li>
+  <li><a href="https://whatwg.org/code-of-conduct">Code of Conduct</a></li>
   <?php wp_list_pages('title_li='); ?>
  </ul>
-			
+
  <h2>Archives</h2>
  <ul>
   <?php wp_get_archives('type=monthly'); ?>
