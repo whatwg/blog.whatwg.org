bb10b839273809d1dc4311745fe51cd5b2072159
Domenic Denicola <d@domenic.me>
2021-05-18 10:59:59 -0400

    Add comment

--- org.whatwg.awesome/functions.php
+++ org.whatwg.awesome/functions.php
@@ -8,6 +8,8 @@ remove_filter('the_content', 'wptexturize');
 remove_filter('the_excerpt', 'wpautop');
 remove_filter('the_excerpt', 'wptexturize');
 
+// Keep (mostly) in sync with https://github.com/whatwg/misc-server and
+// https://github.com/whatwg/participate.whatwg.org.
 add_action('send_headers', function() {
   header('strict-transport-security', 'max-age=63072000; includeSubDomains; preload');
   header('x-content-type-options', 'nosniff');
